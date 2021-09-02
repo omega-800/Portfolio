4c150d063157eb71f3d940c0f90951bb7d90bfad
omega-800 <50942480+omega-800@users.noreply.github.com>
2021-09-03 01:09:56 +0200

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-import firebase from 'firebase'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
